2ecbc165cc82fb0abfa03c660f83c028eea5b6aa
suele <suelen.ads.guimaraes@gmail.com>
2021-05-07 21:17:29 -0300

    Add entity Phone.

---
+++ src/main/java/com/example/myFirstProject/entities/Phone.java
@@ -0,0 +1,27 @@
+package com.example.myFirstProject.entities;
+
+import com.example.myFirstProject.enums.PhoneType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PhoneType type;
+
+    @Column(nullable = false)
+    private String number;
+
+}
