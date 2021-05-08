a79dfe96e0f60ed8d182d545d756a9709ec8f256
suele <suelen.ads.guimaraes@gmail.com>
2021-05-07 21:39:38 -0300

    Add lombok not to insert getter and setter.

--- src/main/java/com/example/myFirstProject/entities/Category.java
+++ src/main/java/com/example/myFirstProject/entities/Category.java
@@ -1,69 +1,32 @@
 package com.example.myFirstProject.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Category implements Serializable {
+
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 50)
     private String name;
 
-    @JsonIgnore
-    @OneToMany(mappedBy = "category")
-    private List<Product> products = new ArrayList<>();
+    @JsonIgnoreProperties("products")
+    @OneToMany(fetch = FetchType.LAZY,cascade = {CascadeType.ALL, CascadeType.REMOVE})
+    private List<Product> products;
 
-    public Category () {
-
-    }
-
-
-    public Category (Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId () {
-        return id;
-    }
-
-    public void setId (Long id) {
-        this.id = id;
-    }
-
-    public String getName () {
-        return name;
-    }
-
-    public void setName (String name) {
-        this.name = name;
-    }
-
-    public List<Product> getProducts () {
-        return products;
-    }
-
-    @Override
-    public boolean equals (Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Category category = (Category) o;
-
-        if (!id.equals(category.id)) return false;
-        if (!name.equals(category.name)) return false;
-        return products.equals(category.products);
-    }
-
-    @Override
-    public int hashCode () {
-        return id.hashCode();
-    }
 }
--- src/main/java/com/example/myFirstProject/entities/Product.java
+++ src/main/java/com/example/myFirstProject/entities/Product.java
@@ -1,9 +1,16 @@
 package com.example.myFirstProject.entities;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Product implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -11,54 +18,14 @@ public class Product implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 50)
     private String name;
+
+    @Column(nullable = false)
     private Double price;
 
     @ManyToOne
-    @JoinColumn(name= "category_id")
+    @JoinColumn(name = "category_id")
     private Category category;
-
-    public Product () {
-    }
-
-    public Product (Long id, String name, Double price, Category category) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-    }
-
-    public Long getId () {
-        return id;
-    }
-
-    public String getName () {
-        return name;
-    }
-
-    public Double getPrice () {
-        return price;
-    }
-
-
-    public Category getCategory () {
-        return category;
-    }
-
-    @Override
-    public boolean equals (Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Product product = (Product) o;
-
-        if (!id.equals(product.id)) return false;
-        if (!name.equals(product.name)) return false;
-        if (!price.equals(product.price)) return false;
-        return category.equals(product.category);
-    }
-
-    @Override
-    public int hashCode () {
-        return id.hashCode();
-    }
 }
