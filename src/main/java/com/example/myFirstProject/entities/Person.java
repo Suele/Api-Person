312bec9ecdc52fc1f72721d1af29bb42ea4fb487
suele <suelen.ads.guimaraes@gmail.com>
2021-05-07 21:16:15 -0300

    Add entity Person.

---
+++ src/main/java/com/example/myFirstProject/entities/Person.java
@@ -0,0 +1,36 @@
+package com.example.myFirstProject.entities;
+
+import com.example.myFirstProject.enums.PhoneType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    private LocalDate birthDate;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    private List<Phone> phones;
+
+}
